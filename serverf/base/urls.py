687f6690cad53ac39279c719040a134a2bd5ef7c
Uze1ess <102526759+Uze1ess@users.noreply.github.com>
2024-12-07 23:31:28 +0700

    Delete serverf directory

--- serverf/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [ 
-    path('', views.home, name="home"),
-    # path('train/', views.train, name="train"),
-    path('api/post-student-data/', views.post_student_data, name='save_student_data'),
-]
